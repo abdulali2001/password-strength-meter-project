8051032db19c990f4fa8081d1f9b711ee4b96e96
Abdul Ali <164244793+abdulali2001@users.noreply.github.com>
2025-03-16 23:47:15 +0500

    Update psm.py

--- psm.py
+++ psm.py
@@ -2,7 +2,7 @@ import re
 import streamlit as st
 
 #page styling
-st.set_page_config(page_title="Password Strength Checker By Abdul Ali", page_icon="🔐", layout="centered")
+st.set_page_config(page_title="Password Strength Generator By Abdul Ali", page_icon="🔐", layout="centered")
 #custom css
 st.markdown("""
 <style>
